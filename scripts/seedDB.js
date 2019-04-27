e26f7f0c4419cf58090cb9526255592aef17676a
Himanshu Pandit <himanshu.pandit@outlook.com>
2019-04-27 13:34:15 -0400

    changed seeds file to include test gmail account

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,11 +2,29 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/autoCloud",
+  //process.env.MONGODB_URI || "mongodb://localhost:27017/autoCloud",
+  process.env.MONGODB_URI || "mongodb://heroku_8dt35cjz:bj4mlr99sdsadu140kikke4396@ds159204.mlab.com:59204/heroku_8dt35cjz",
   { useNewUrlParser: true }
 );
 
-const document = [
+const document1 = [
+  {
+    _id: new mongoose.Types.ObjectId(),
+    type: "Insurance",
+    docId: "12345",
+    expirationDate: new Date("2019-05-31"),
+    notificationDay: 7
+  },
+  {
+    _id: new mongoose.Types.ObjectId(),
+    type: "Registration",
+    docId: "ABCDEF",
+    expirationDate: new Date("2019-04-31"),
+    notificationDay: 14
+  }
+];
+
+const document2 = [
   {
     _id: new mongoose.Types.ObjectId(),
     type: "Insurance",
@@ -27,7 +45,7 @@ db.User.remove({})
   .then(() => db.Car.remove({}))
   .then(() => db.Document.remove({}))
   .then(() => {
-    db.Document.insertMany(document, function(err, result) {
+    db.Document.insertMany(document1, function(err, result) {
       if (err) console.log(err);
 
       const car = new db.Car({
@@ -41,11 +59,11 @@ db.User.remove({})
         if (err) return handleError(err);
 
         const user = new db.User({
-          firstName: "User",
-          lastName: "1",
-          userName: "User1",
+          firstName: "Auto",
+          lastName: "Cloud",
+          userName: "AutoCloud",
           password: "12345",
-          email: "User1@user.net",
+          email: "rcbautocloud@gmail.com",
           phoneNumber: "911",
           zipCode: "10001",
           cars: car._id
@@ -54,13 +72,48 @@ db.User.remove({})
         user.save(function(err) {
           if (err) return err;
           //console.log(err);
-        });
+        }); // user.save
+      }); // car.save
+    }); // insertMany
+
+
+    db.Document.insertMany(document2, function(err, result) {
+      if (err) console.log(err);
+
+      const car2 = new db.Car({
+        name: "Honda",
+        model: "Accord",
+        year: 2017,
+        docs: result.map(doc => doc._id)
       });
-    });
-  })
+
+      car2.save(function(err) {
+        if (err) return handleError(err);
+
+        const user2 = new db.User({
+          firstName: "Himanshu",
+          lastName: "Pandit",
+          userName: "hpandit",
+          password: "test123",
+          email: "himanshu.pandit@outlook.com",
+          phoneNumber: "201-918-0080",
+          zipCode: "08816",
+          cars: car2._id
+        });
+
+        user2.save(function(err) {
+          if (err) return err;
+          //console.log(err);
+        }); // user.save
+      }); // car.save
+    }); // insertMany
+
+  }) //db.remove
   .then(() => {
     console.log("success!");
   })
   .catch(err => {
     console.error(err);
   });
+
+  //process.exit();
