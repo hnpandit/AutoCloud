946f9a54ea8d3029beca46f1c7b3cee206f7e38e
HelloWorld <17502830+JVGH@users.noreply.github.com>
2019-04-27 14:16:59 -0400

    updated script file

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 const db = require("../models");
 
 mongoose.connect(
-  //process.env.MONGODB_URI || "mongodb://localhost:27017/autoCloud",
-  process.env.MONGODB_URI || "mongodb://heroku_8dt35cjz:bj4mlr99sdsadu140kikke4396@ds159204.mlab.com:59204/heroku_8dt35cjz",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/autoCloud",
+  //process.env.MONGODB_URI || "mongodb://heroku_8dt35cjz:bj4mlr99sdsadu140kikke4396@ds159204.mlab.com:59204/heroku_8dt35cjz",
   { useNewUrlParser: true }
 );
 
@@ -46,74 +46,66 @@ db.User.remove({})
   .then(() => db.Document.remove({}))
   .then(() => {
     db.Document.insertMany(document1, function(err, result) {
-      if (err) console.log(err);
+      if (err) return err;
 
-      const car = new db.Car({
-        carMake: "Toyota",
+      const car1 = new db.Car({
+        make: "Toyota",
         model: "Rav4",
         year: 2019,
         docs: result.map(doc => doc._id)
       });
 
-      car.save(function(err) {
-        if (err) return handleError(err);
+      car1.save(function(err) {
+        if (err) return err;
 
-        const user = new db.User({
+        const user1 = new db.User({
           firstName: "Auto",
           lastName: "Cloud",
-          userName: "AutoCloud",
-          password: "12345",
           email: "rcbautocloud@gmail.com",
           phoneNumber: "911",
-          zipCode: "10001",
-          cars: car._id
+          cars: car1._id
         });
 
-        user.save(function(err) {
+        user1.save(function(err) {
           if (err) return err;
-          //console.log(err);
-        }); // user.save
-      }); // car.save
-    }); // insertMany
-
-
+        });
+      });
+    });
+  })
+  .then(() => {
     db.Document.insertMany(document2, function(err, result) {
-      if (err) console.log(err);
+      if (err) return err;
 
       const car2 = new db.Car({
-        name: "Honda",
+        make: "Honda",
         model: "Accord",
         year: 2017,
         docs: result.map(doc => doc._id)
       });
 
       car2.save(function(err) {
-        if (err) return handleError(err);
+        if (err) return err;
 
         const user2 = new db.User({
           firstName: "Himanshu",
           lastName: "Pandit",
-          userName: "hpandit",
-          password: "test123",
           email: "himanshu.pandit@outlook.com",
           phoneNumber: "201-918-0080",
-          zipCode: "08816",
           cars: car2._id
         });
 
         user2.save(function(err) {
           if (err) return err;
-          //console.log(err);
-        }); // user.save
-      }); // car.save
-    }); // insertMany
-
-  }) //db.remove
+        });
+      });
+    });
+  })
   .then(() => {
     console.log("success!");
+    //mongoose.disconnect();
+    //process.exit(0);
   })
   .catch(err => {
     console.error(err);
+    process.exit(1);
   });
-
-  //process.exit();
