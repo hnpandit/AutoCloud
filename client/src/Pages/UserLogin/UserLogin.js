a135f02977a26963f24b1d1792f32b0e03eeb22c
jonp182015 <jonp182015@gmail.com>
2019-05-01 13:10:49 -0400

    added onSignIn method

--- client/src/Pages/UserLogin/UserLogin.js
+++ client/src/Pages/UserLogin/UserLogin.js
@@ -1,19 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { Redirect } from "react-router-dom";
-import './UserLogin.css';
+import "./UserLogin.css";
 
 /* global gapi */
 
 class UserLogin extends Component {
-	
   state = {
     userEmail: sessionStorage.getItem("email"),
     redirect: false
-  }
+  };
 
   componentDidMount() {
-    if(this.state.userEmail){
-      this.setRedirect()
+    if (this.state.userEmail) {
+      //this.setRedirect()
     }
   }
 
@@ -24,9 +23,10 @@ class UserLogin extends Component {
   };
 
   renderRedirect = () => {
-    if (this.state.redirect && this.state.userEmail) {
+    console.log("logging this.state.userEmail: ", this.state.userEmail);
+    if (this.state.userEmail) {
       return <Redirect to="/dashboard" />;
-    } else  {
+    } else {
       return <Redirect to="/register" />;
     }
   };
@@ -43,49 +43,57 @@ class UserLogin extends Component {
     console.log(this.state);
   };
   */
-  
+  onSignIn = googleUser => {
+    var profile = googleUser.getBasicProfile();
+    console.log("ID: " + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    console.log("Name: " + profile.getName());
+    console.log("Image URL: " + profile.getImageUrl());
+    console.log("Email: " + profile.getEmail()); // This is null if the 'email' scope is not present.
+    sessionStorage.setItem("email", profile.getEmail());
+    this.renderRedirect();
+  };
 
   //Added to remove cookies from browser
- removeCookies = () => {
-  var res = document.cookie;
-  var multiple = res.split(";");
-  for (var i = 0; i < multiple.length; i++) {
-    var key = multiple[i].split("=");
-    document.cookie = key[0] + " =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
-  }
- };	 
-	 
+  removeCookies = () => {
+    var res = document.cookie;
+    var multiple = res.split(";");
+    for (var i = 0; i < multiple.length; i++) {
+      var key = multiple[i].split("=");
+      document.cookie = key[0] + " =; expires = Thu, 01 Jan 1970 00:00:00 UTC";
+    }
+  };
+
   signOut = () => {
     var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function () {
+    auth2.signOut().then(function() {
       console.log("User signed out.");
     });
     this.removeCookies();
     sessionStorage.removeItem("email");
-  };  
+  };
 
-render() {
-  return (
-    <div id="registrationPage">
-      <div className="container">
-        <div className="row">
-          <div className="col-lg-12">
-            <h3>Welcome to </h3>
-            <h1>Auto Cloud</h1>
-            <div 
-              onClick={this.setRedirect}
-              className="g-signin2"
-              id="google-btn"
-              data-onsuccess="onSignIn"
-            />
-            {this.renderRedirect()}
-            <br/>
-            {/*Added signout button for testing purposes*/}
-            <a href="/" onClick={this.signOut}>
-              Sign out
-							</a>
-            {/* <p>Log in</p> */}
-            {/*
+  render() {
+    return (
+      <div id="registrationPage">
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-12">
+              <h3>Welcome to </h3>
+              <h1>Auto Cloud</h1>
+              <div
+                onClick={this.setRedirect}
+                className="g-signin2"
+                id="google-btn"
+                data-onsuccess="onSignIn"
+              />
+              {this.renderRedirect()}
+              <br />
+              {/*Added signout button for testing purposes*/}
+              <a href="/" onClick={this.signOut}>
+                Sign out
+              </a>
+              {/* <p>Log in</p> */}
+              {/*
               <form>
                 <div className="form-group">
                   <input
@@ -122,12 +130,12 @@ render() {
               </form>
               <a href="/register">Register</a>
               */}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
 }
 
 export default UserLogin;
