1f528e9d73355ee13a62999a776e8c5329b8e8b2
DS1R3S <dsires13@mac.com>
2019-05-07 14:23:43 -0400

    readme

--- client/src/Pages/Dashboard/Dashboard.js
+++ client/src/Pages/Dashboard/Dashboard.js
@@ -1,80 +1,47 @@
 import React, { Component } from "react";
-// import "../../App.css";
 import "./Dashboard.css";
 import API from "../../util/api";
-
-import Tab from "react-bootstrap/Tab";
-import Tabs from "react-bootstrap/Tabs";
+import Vehicles from "../../Components/Vehicle/Vehicle";
 import Navbar from "../../Components/Navbar/Navbar";
-// import Footer from "../../Components/Footer/Footer";
-// import Col from 'react-bootstrap/Col'
-// import Nav from 'react-bootstrap/Nav'
-// import Row from 'react-bootstrap/Row'
 
 class Dashboard extends Component {
+  
   state = {
-    userEmail: sessionStorage.getItem("email"),
     user: []
   };
 
   componentDidMount() {
-    if (this.state.userEmail) {
-      this.loadItems();
-    }
-  }
-
-  loadItems = () => {
-    API.getUserByEmail(this.state.userEmail)
-      .then(res => {
-        this.setState({
-          user: res.data
-        });
-      })
+    API.getUsers()
+      .then(res => this.setState({user: res.data}))
       .catch(err => console.log(err));
-    console.log("logging this.state: ", this.state);
+
   };
+
+  // loadItems = () => {
+  //   API.getUserByEmail(this.state.userEmail)
+  //     .then(res => {
+  //       this.setState({
+  //         user: res.data
+  //       });
+  //     })
+  //     .catch(err => console.log(err));
+  //   console.log("logging this.state: ", this.state);
+  // };
   render() {
     return (
-      <div>
+     
+       <div>
         <Navbar />
         <div>
-          <Tabs defaultActiveKey="" id="dashboard-tabs">
-            <Tab eventKey="home" title="Vehicle 1">
-              <div>
-                <Tab.Pane eventKey="first">
-                  <h1>Vehicle #1</h1>
-                  <div className="card">
-                    <p>Registration Expiration</p>
-                  </div>
-                  <div className="card">
-                    <p>License Expiration</p>
-                  </div>
-                  <div className="card">
-                    <p>Inspection Expiration</p>
-                  </div>
-                </Tab.Pane>
-              </div>
-            </Tab>
-            <Tab eventKey="profile" title="Vehicle 2">
-              <div>
-                <Tab.Pane eventKey="second">
-                  <h1>Vehicle #2</h1>
-                  <div className="card">
-                    <p>Registration Expiration</p>
-                  </div>
-                  <div className="card">
-                    <p>License Expiration</p>
-                  </div>
-                  <div className="card">
-                    <p>Inspection Expiration</p>
-                  </div>
-                </Tab.Pane>
-              </div>
-            </Tab>
-          </Tabs>
+         
+            
+            <Vehicles
+              vehicles={this.state.user}
+            />
+
+            
         </div>
-      </div>
-  
+      </div> 
 
     );
   }
@@ -82,24 +49,4 @@ class Dashboard extends Component {
 
 export default Dashboard;
 
-    {/* <div>
-        <Navbar />
-        <div>
-         
-            
-            <Vehicles
-              vehicles={[{
-                registration: '10',
-                license: '11',
-                inspection: '12'
-              },
-              {
-                registration: '15',
-                license: '4',
-                inspection: '5'
-              },]}
-            />
 
-            
-        </div>
-      </div> */}
